d13a3b58adea56fb232a009d04bd6399cca181c4
BorMax73 <Email>
2021-01-07 23:27:19 +0200

    beta version

---
+++ Tires1.01/tire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tires1._01
+{
+    class Tire
+    {
+        private double width { get; set; }
+        private double sidewall { get; set; }
+        private double diameter { get; set; }
+        private string brand { get; set; }
+        private string season { get; set; }
+
+        public Tire()
+        {
+
+        }
+    }
+}
